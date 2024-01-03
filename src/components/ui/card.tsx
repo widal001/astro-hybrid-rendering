4c2d8c4b12e5a764b150ff374636dcebe22b97e8
widal001 <williamdaly422@gmail.com>
2024-01-02 20:46:55 -0500

    refactor: Replaces Card.astro with CardItem.astro This new item uses the shadcn card.tsx component

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -2,18 +2,17 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
+const defaultCardClassNames = `
+  rounded-lg border bg-card text-card-foreground shadow-sm
+  hover:shadow-md hover:-translate-y-1
+  focus:shadow-md focus:-translate-y-1
+`;
+
 const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
-      className
-    )}
-    {...props}
-  />
+  <div ref={ref} className={cn(defaultCardClassNames, className)} {...props} />
 ));
 Card.displayName = "Card";
 
